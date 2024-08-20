32570a4cc8355dcc52d64253c47d0574932da135
Osama Mohamed <osamamohamed1542@gmail.com>
2024-08-21 00:51:19 +0300

    Content Negotiation And Custom CSV Fromatter

--- Shared/DataTransferObjects/CompanyDto.cs
+++ Shared/DataTransferObjects/CompanyDto.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Shared.DataTransferObjects
 {
-    public record CompanyDto(Guid Id, string Name, string FullAddress)
+    public record CompanyDto()
     {
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public string? FullAddress { get; init; }
     }
 }
